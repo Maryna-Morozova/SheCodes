3fbce07ec58befe8dbc78968e6e74b59a3493100
Morozova Maryna <morozova.maryna@gmail.com>
2022-10-04 00:20:44 +0300

    Chanched position of currently forecast with geolocation

--- src/index.js
+++ src/index.js
@@ -25,26 +25,53 @@ currentDate.innerHTML = `${week[day]} ${hours}:${minutes}`;
 
 
 
-// Current geolocation in h1
+// Current geolocation
+
+let apiKey = "d9fd12d82698dc44978f38d20ae7d12d";
+let lat = null;
+let lon = null;
 
 navigator.geolocation.getCurrentPosition(showPosition);
-
 function showPosition(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-
-    let apiKey = "d9fd12d82698dc44978f38d20ae7d12d";
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    let apiUrl2 = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     axios.get(apiUrl).then(displayTemp);
+    axios.get(apiUrl2).then(displayGeo);
 }
 
-function displayTemp(response) {
-    console.log(response.data);
-    let h1 = document.querySelector("h1");
-    let superText = "o";
-    h1.innerHTML = `Currently ${Math.round(response.data.main.temp)} ${superText.sup()}C in ${response.data.name}`;
+function displayTemp(response) {    
+    let currentlyTemp = document.querySelector(".currently");
+    currentlyTemp.innerHTML = `${Math.round(response.data.main.temp)}`;
+
+    let currentIcon = document.querySelector("#icons");
+    currentIcon.setAttribute("src", `${iconChange(response.data.weather[0].main)}`);
+    function iconChange(main) {
+        if (weatherIcons[main] !== undefined) {
+            return `${weatherIcons[main]}`
+        }
+    }
+
+    let fahrenheit = document.querySelector(".fahrenheit");
+    fahrenheit.addEventListener("click", convertFahrenheit);
+    function convertFahrenheit(event) {
+        event.preventDefault();
+        currentlyTemp.innerHTML = `${Math.round((response.data.main.temp) * 9 / 5 + 32)}`
+    }
+
+    let celsius = document.querySelector(".celsius");
+    celsius.addEventListener("click", convertCelsius);
+    function convertCelsius(event) {
+        event.preventDefault();
+        currentlyTemp.innerHTML = `${Math.round(response.data.main.temp)}`
+    }
 }
 
+function displayGeo(response) {
+    let currentGeo = document.querySelector("#search-text-input");
+    currentGeo.setAttribute("placeholder", `${response.data[0].name}`);
+}
 
 
 // Input //
@@ -54,8 +81,6 @@ submitTheForm.addEventListener("submit", search)
 
 let searchInput = document.querySelector("#search-text-input");
 searchInput.addEventListener("click", function () { searchInput.value = `` })
-
-var apiKey = "d9fd12d82698dc44978f38d20ae7d12d"
 
 function search(event) {
     event.preventDefault();
@@ -67,24 +92,24 @@ function search(event) {
 
 
 let weatherIcons = {
-        Thunderstorm: "images/weather-icons-png/CloudRainThunder.png",
-        Drizzle: "images/weather-icons-png/IsoRainSwrsDay.png",
-        Rain: "images/weather-icons-png/FreezingRain.png",
-        Snow: "images/weather-icons-png/OccLightSnow.png",
-        Clear: "images/weather-icons-png/Sunny.png",
-        Clouds: "images/weather-icons-png/PartlyCloudyDay.png",  
-        
-        Mist: "images/weather-icons-png/Mist.png",
-        Smoke: "images/weather-icons-png/Mist.png",
-        Haze: "images/weather-icons-png/Mist.png",
-        Dust: "images/weather-icons-png/Mist.png",
-        Fog: "images/weather-icons-png/Mist.png",
-        Sand: "images/weather-icons-png/Mist.png",
-        Dust: "images/weather-icons-png/Mist.png",
-        Ash: "images/weather-icons-png/Mist.png",
-        Squall: "images/weather-icons-png/Mist.png",
-        
-        Tornado: "images/weather-icons-png/wind.png",
+    Thunderstorm: "images/weather-icons-png/CloudRainThunder.png",
+    Drizzle: "images/weather-icons-png/IsoRainSwrsDay.png",
+    Rain: "images/weather-icons-png/FreezingRain.png",
+    Snow: "images/weather-icons-png/OccLightSnow.png",
+    Clear: "images/weather-icons-png/Sunny.png",
+    Clouds: "images/weather-icons-png/PartlyCloudyDay.png",
+
+    Mist: "images/weather-icons-png/Mist.png",
+    Smoke: "images/weather-icons-png/Mist.png",
+    Haze: "images/weather-icons-png/Mist.png",
+    Dust: "images/weather-icons-png/Mist.png",
+    Fog: "images/weather-icons-png/Mist.png",
+    Sand: "images/weather-icons-png/Mist.png",
+    Dust: "images/weather-icons-png/Mist.png",
+    Ash: "images/weather-icons-png/Mist.png",
+    Squall: "images/weather-icons-png/Mist.png",
+
+    Tornado: "images/weather-icons-png/wind.png",
 };
 
 
@@ -108,10 +133,8 @@ function display(response) {
         currentlyForecast.innerHTML = `${Math.round(response.data.main.temp)}`
     }
 
-    let icon = document.querySelector("#icon");
+    let icon = document.querySelector("#icons");
     icon.setAttribute("src", `${iconChange(response.data.weather[0].main)}`);
-    console.log(response.data.weather[0].main);// clouds
-
     function iconChange(main) {
         if (weatherIcons[main] !== undefined) {
             return `${weatherIcons[main]}`
